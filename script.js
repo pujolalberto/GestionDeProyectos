b2a86ae15ea3a3bb4cd38db0b8812204e9459fea
Nicolas Pujol <esc.pujolalberto@gmail.com>
2024-05-06 12:23:35 -0300

    delete

--- script.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-const counterDisplay = document.getElementById('counter');
-const clickButton = document.getElementById('clickButton');
-
-clickButton.addEventListener('click', () => {
-  count++;
-  counterDisplay.textContent = count;
-});
